7c2c30b902aca9e3f357978d36383fc1991b2a88
Duy Nguyen <duynk198@gmail.com>
2022-10-29 12:16:55 +0700

    depend : setup alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          assets: './src/assets',
+          components: '.src/components',
+          navigators: '.src/navigators',
+          screens: './src/screens',
+          services: '.src/services',
+          store: '.src/store',
+          utils: '.src/utils',
+        },
+      },
+    ],
+  ],
 };
