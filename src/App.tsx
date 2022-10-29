0eaf3ebc420f55b562d04c4335d1892f7f6094a5
Duy Nguyen <duynk198@gmail.com>
2022-10-29 11:31:51 +0700

    depend : add redux for state management

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { navigationRef } from './navigators/utils';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import AppNavigators from './navigators';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <NavigationContainer ref={navigationRef}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppNavigators />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer ref={navigationRef}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppNavigators />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
