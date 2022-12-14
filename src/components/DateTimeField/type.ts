d1290256c5a981b182ce0e22e779e6b89fcac2e5
Duy Nguyen <duynk198@gmail.com>
2022-12-14 22:40:00 +0700

    chore : change name

--- src/components/DateTimeField/type.ts
+++
@@ -1,8 +0,0 @@
-import { Control } from 'react-hook-form';
-import { IOSNativeProps } from '@react-native-community/datetimepicker';
-
-type RNDateTimePicker = IOSNativeProps;
-export interface IDateTimeFieldProps extends RNDateTimePicker {
-  name: any;
-  control: Control<any, any>;
-}
